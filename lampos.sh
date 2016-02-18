6afa7a6f45fa20ed44f07404fb06d642fb73dab1
Juan Fernando Villa Hernández <juan.villa@paisdelconocimiento.org>
2016-02-18 15:19:21 -0500

    Create lampos.sh

---
+++ lampos.sh
@@ -0,0 +1,42 @@
+# Ubuntu LAMP stack with Apache, MariaDB, PHP, and SSL
+
+FROM ubuntu
+
+MAINTAINER DockerFan version 1.0
+
+ENV DEBIAN_FRONTEND noninteractive
+
+ENV APACHE_RUN_USER www-data
+ENV APACHE_RUN_GROUP www-data
+ENV APACHE_LOG_DIR /var/log/apache2
+ENV APACHE_LOCK_DIR /var/lock/apache2
+ENV APACHE_PID_FILE /var/run/apache2.pid
+
+# Install Apache, SSL, PHP, and some PHP modules
+
+RUN apt-get update && apt-get install -y apache2 \
+ openssl \
+ php5 \
+ php5-cli \
+ php5-apcu
+ 
+# Install MariaDB and set default root password
+
+RUN echo 'mariadb-server mariadb-server/root_password  password mypassword' | debconf-set-selections
+RUN echo 'mariadb-server mariadb-server/root_password_again password mypassword' | debconf-set-selections
+RUN apt-get install mariadb-server -y
+
+# Disable the default Apache site config
+# Install your site's Apache configuration and activate SSL
+
+ADD my_apache.conf /etc/apache2/sites-available/
+RUN a2dissite 000-default
+RUN a2ensite my_apache
+RUN a2enmod ssl
+
+# Remove APT files
+RUN apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+
+EXPOSE 443 8080
+
+CMD ["/usr/sbin/apache2ctl", "-D", "FOREGROUND"]
