1192ebe5718e6b4a7304168998ca209706a89399
Juan Fernando Villa Hernández <juan.villa@paisdelconocimiento.org>
2016-09-09 02:15:22 -0500

    Create swap-extra.sh

---
+++ swap-extra.sh
@@ -0,0 +1,7 @@
+#http://askubuntu.com/questions/178712/how-to-increase-swap-space
+sudo mkdir /media/fasthdd/
+sudo dd if=/dev/zero of=/media/fasthdd/swapfile.img bs=1024 count=1M
+sudo mkswap /media/fasthdd/swapfile.img
+sudo echo "/media/fasthdd/swapfile.img swap swap sw 0 0" >> /etc/fstab
+sudo nano /etc/fstab
+sudo swapon /media/fasthdd/swapfile.img
