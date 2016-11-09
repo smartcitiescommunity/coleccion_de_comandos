f57a162be8131e6c14f46995382cd536b70f04f7
Juan Fernando Villa Hernández <juan.villa@paisdelconocimiento.org>
2016-11-08 23:35:56 -0500

    Create tts-env.sh

---
+++ tts-env.sh
@@ -0,0 +1,8 @@
+sudo apt-get install gnustep-gui-runtime
+say "hello"
+sudo apt-get install festival
+echo "hello" | festival --tts
+sudo apt-get install speech-dispatcher
+spd-say "hello"
+sudo apt-get install espeak
+espeak "hello"
