56873176b89d43ed690ee5ff652a144f07457aa2
Juan Fernando Villa Hernández <juan.villa@paisdelconocimiento.org>
2016-11-10 11:17:32 -0500

    Create virtualenvpython.sh

---
+++ virtualenvpython.sh
@@ -0,0 +1,9 @@
+sudo pip install virtualenv 
+mkdir virtualenv.python
+#which python
+virtualenv -p /usr/bin/pythonx.x .env1
+source .env1/bin/activate
+#which python
+python
+#for deactivate
+deactivate
