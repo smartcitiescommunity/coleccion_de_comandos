68695d60067e080b6262d7c6375eb76dda347550
Juan Fernando Villa Hernández <juan.villa@paisdelconocimiento.org>
2016-11-28 20:22:16 -0500

    Create socat2_server.sh

---
+++ socat2_server.sh
@@ -0,0 +1,1 @@
+socat -T 1 -d -d tcp-l:10081,reuseaddr,fork,crlf system:"echo -e \"\\\"HTTP/1.0 200 OK\\\nDocumentType: text/html\\\n\\\n<html>date: \$\(date\)<br>server:\$SOCAT_SOCKADDR:\$SOCAT_SOCKPORT<br>client: \$SOCAT_PEERADDR:\$SOCAT_PEERPORT\\\n<pre>\\\"\"; cat; echo -e \"\\\"\\\n</pre></html>\\\"\""
