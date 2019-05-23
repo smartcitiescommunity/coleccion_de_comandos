d29bcfe7b54aeb6afeb910d44f09549caf116b89
Juan Fernando Villa Hernández <juan.villa@paisdelconocimiento.org>
2019-05-23 17:01:42 -0500

    Update swap-extra.sh

--- swap-extra.sh
+++ swap-extra.sh
@@ -5,3 +5,18 @@ sudo mkswap /media/fasthdd/swapfile.img
 sudo echo "/media/fasthdd/swapfile.img swap swap sw,pri=9999 0 0" >> /etc/fstab
 sudo nano /etc/fstab
 sudo swapon /media/fasthdd/swapfile.img
+sudo nano /usr/local/sbin/swap2ram.sh
+##!/bin/sh
+#
+#mem=$(LC_ALL=C free  | awk '/Mem:/ {print $4}')
+#swap=$(LC_ALL=C free | awk '/Swap:/ {print $3}')
+#
+#if [ $mem -lt $swap ]; then
+#    echo "ERROR: not enough RAM to write swap back, nothing done" >&2
+#    exit 1
+#fi
+#
+swapoff -a &&
+swapon -a
+sudo chmod +x /usr/local/sbin/swap2ram.sh
+sudo /usr/local/sbin/swap2ram.sh
